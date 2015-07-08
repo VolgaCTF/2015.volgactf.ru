cd3dc615d93cff8aea5257be9be87de27b9a37d5
volalex <a.volkov@myplaycity.com>
2015-07-08 17:09:40 +0400

    First header

---
+++ Gruntfile.js
@@ -0,0 +1,102 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        copy: {
+            main: {
+                src: ['*.html', 'img/**', 'less/**'],
+                dest: 'dist/'
+            },
+            jquery: {
+                files: [{
+                    expand: true,
+                    cwd: 'bower_components/jquery/dist/',
+                    src: [
+                        'jquery.js',
+                        'jquery.min.js'
+                    ],
+                    dest: 'dist/js/'
+                }]
+            },
+            bootstrap: {
+                files: [{
+                    expand: true,
+                    cwd: 'bower_components/bootstrap/dist/',
+                    src: [
+                        'css/bootstrap.css',
+                        'css/bootstrap.min.css',
+                        'js/bootstrap.js',
+                        'js/bootstrap.min.js'
+                    ],
+                    dest: 'dist/'
+                }]
+            }
+        },
+        less: {
+            expanded: {
+                options: {
+                    paths: ["css"]
+                },
+                files: {
+                    "dist/css/<%= pkg.name %>.css": "less/<%= pkg.name %>.less"
+                }
+            },
+            minified: {
+                options: {
+                    paths: ["css"],
+                    cleancss: true
+                },
+                files: {
+                    "dist/css/<%= pkg.name %>.min.css": "less/<%= pkg.name %>.less"
+                }
+            }
+        },
+
+        banner: '/*!\n' +
+        ' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+        ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+        ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n' +
+        ' */\n',
+
+        usebanner: {
+            dist: {
+                options: {
+                    position: 'top',
+                    banner: '<%= banner %>'
+                },
+                files: {
+                    src: ['dist/css/<%= pkg.name %>.css', 'dist/css/<%= pkg.name %>.min.css', 'dist/js/<%= pkg.name %>.js', 'dist/js/<%= pkg.name %>.min.js']
+                }
+            }
+        },
+        watch: {
+            copy: {
+                files: ['*.html', 'mail/**', 'img/**', 'less/**'],
+                tasks: ['copy'],
+                options: {
+                    spawn: false
+                }
+            },
+            less: {
+                files: ['less/*.less'],
+                tasks: ['less'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+    });
+
+    // Load the plugins.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-banner');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+    grunt.registerTask('default', ['copy', 'less', 'usebanner']);
+
+};
